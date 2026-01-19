739c4e21ee0f5fbadd22aaa447df76a44185c6ec
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2026-01-20 01:39:34 +0530

    Refactor app structure and remove WelcomePage
    Replaced the WelcomePage with a default Flutter counter app in main.dart. Refactored constant.dart to update style and color classes. Removed the now-unused welcome_page.dart.

--- task_2/lib/data/constant.dart
+++ task_2/lib/data/constant.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class KTextStyle{
+class RText {
   static final TextStyle titleStyle = GoogleFonts.poppins(
     fontSize: 32,
     fontWeight: FontWeight.w700,
-    color: Color.fromRGBO(255, 255, 255, 1)
-    
+    color: Color.fromRGBO(255, 255, 255, 1),
+  );
+}
+
+class RColors {
+  static const Color primaryColor = Color.fromRGBO(255, 255, 255, 1);
+  static const Color morningButton = Color.fromRGBO(243, 210, 23, 1);
+  static const Color afternoonButton = Color.fromRGBO(48, 193, 255, 1);
+  static const LinearGradient gradientMorning = LinearGradient(
+    colors: [Color.fromRGBO(243, 210, 23, 1), Color.fromRGBO(255, 48, 241, 1)],
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+  );
+  static const LinearGradient gradientAfternoon = LinearGradient(
+    colors: [Color.fromRGBO(243, 210, 23, 1), Color.fromRGBO(48, 193, 255, 1)],
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
   );
 }
