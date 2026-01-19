caea00e80005916a97af79adff4ac2f8d25da6cd
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2026-01-19 14:37:07 +0530

    Add welcome page and custom text style
    Introduced WelcomePage as the new home screen with a custom title text style using Google Fonts. Updated main.dart to use WelcomePage and set a dark scaffold background. Added constant.dart for reusable text styles and updated dependencies to include google_fonts.

---
+++ task_2/lib/data/constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class KTextStyle{
+  static final TextStyle titleStyle = GoogleFonts.poppins(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    color: Color.fromRGBO(255, 255, 255, 1)
+    
+  );
+}
